bf30e811e3f53f183bbdf45640ec119572510da4
Broms <patrikbroms@kamk.fi>
2019-01-15 06:20:02 +0200

    clean up

--- socekts/build.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-clear
-cd ./DebugBin
-gcc ../main.c -Iinclude -I../../PakkiUtils -g -Wall -fexceptions -lm -lpthread -lrt -lm -ldl -o ENGINE
-cd ../
-echo hello
-
--- socekts/run.sh
+++
@@ -1,2 +0,0 @@
-clear
-./DebugBin/ENGINE
